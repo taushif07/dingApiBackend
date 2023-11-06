adfb9f238020f8545fd888fead73562a12c4b9a9
Taushif Ansari <taushif@ezygentechnology.com>
2023-11-06 21:11:18 +0530

    add complete ding api get routes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 const https = require("https");
 const dotenv = require('dotenv').config();
 var cors = require('cors');
-
+const axios = require('axios');
+const instance = require('./dingInstance');
 
 
 const port = process.env.PORT || 3000;
@@ -108,6 +109,164 @@ app.get('/ding-access-token', function (request, response) {
         .catch(() => {
             response.status(500);
         });
+});
+
+app.get("/GetErrorCodeDescriptions", async function (request, response) {
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetErrorCodeDescriptions`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetAccountLookup", async function (request, response) {
+    const accountNumber = "916203962194";
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetAccountLookup?accountNumber=${accountNumber}`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetPromotionDescriptions", async function (request, response) {
+    const languageCodes = "en"
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetPromotionDescriptions?languageCodes=${languageCodes}`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+
+app.get("/GetPromotions", async function (request, response) {
+    const countryIsos = "IN";
+    const providerCodes = "RJIN";
+    const accountNumber = "916203962194";
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetPromotions?countryIsos=${countryIsos}&providerCodes=${providerCodes}&accountNumber=${accountNumber}`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetBalance", async function (request, response) {
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetBalance`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetProductDescriptions", async function (request, response) {
+    const skuCodes = "RJININ80851"
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetProductDescriptions?skuCodes=${skuCodes}`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetProducts", async function (request, response) {
+    const providerCodes = "RJIN";
+    const countryIsos = "IN";
+    const regionCodes = "IN";
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetProducts?countryIsos=${countryIsos}&providerCodes=${providerCodes}&regionCodes=${regionCodes}`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetProviderStatus", async function (request, response) {
+    const providerCodes = "RJIN"
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetProviderStatus?providerCodes=${providerCodes}`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetProviders", async function (request, response) {
+    const countryIsos = "IN";
+    const regionCodes = "IN";
+    const accountNumber = "916203962194";
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get(`/api/V1/GetProviders?countryIsos=${countryIsos}&regionCodes=${regionCodes}&accountNumber=${accountNumber}`);
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetCurrencies", async function (request, response) {
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get('/api/V1/GetCurrencies');
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetRegions", async function (request, response) {
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get('/api/V1/GetRegions');
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }    
+});
+
+app.get("/GetCountries", async function (request, response) {
+    console.log(`[GET] ${request.url}`);
+    try {
+        const res = await instance.get('/api/V1/GetCountries');
+        const data = res.data;
+        response.json(data);
+      } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: 'Internal Server Error' });
+      }
 });
 
 var server2 = app.listen(port, function () {
---
+++ dingInstance.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+const getToken = async() => await axios.get(`${process.env.DING_CONNECT_BACKEND}/ding-access-token`).then((res) => {
+    console.log("token",res?.data?.access_token);
+    return res?.data?.access_token;
+});
+
+const instance = axios.create({
+    
+    baseURL: `${process.env.DING_BASE_URL}`,
+
+});
+
+
+instance.interceptors.request.use(
+    async (config) => {
+
+        await getToken().then(res => {
+            config.headers.Authorization = `Bearer ${res}`;
+            config.headers.Accept = "application/json";
+        });
+        console.log("config",config.headers);
+        return config;
+    },
+    error => Promise.reject(error)
+)
+
+instance.interceptors.response.use(
+    response => {
+        return response
+    },
+    error => {
+        if ([401].includes(error.response.status)) {
+            console.log("Invalid token or expired token.");
+        }
+
+        return Promise.reject(error)
+    }
+)
+
+module.exports = instance;
