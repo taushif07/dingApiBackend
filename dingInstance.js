6ee28c4686c62303ecf33909ae86107d1047dc9a
Taushif Ansari <taushif@ezygentechnology.com>
2023-11-20 23:09:12 +0530

    add

--- dingInstance.js
+++ dingInstance.js
@@ -9,7 +9,7 @@ const instance = axios.create({
     
     baseURL: `${process.env.DING_BASE_URL}`,
     headers: {
-    "api_key": "Hkc9DU33o305tCnnJx2qeX"
+    "api_key": "EOKdP4ZyM2Y5hEkOv9mS5i"
     }
 
 });
