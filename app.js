8ea90d898db07c1fc8014ca4b2f64dbd8aefc234
Taushif Ansari <taushif@ezygentechnology.com>
2023-12-21 23:37:29 +0530

    fix

--- app.js
+++ app.js
@@ -230,103 +230,14 @@ app.post("/shopping/flight-offers/pricing", async function (request, response) {
 
 app.post("/booking/flight-orders", async function (request, response) {
   const flightOfferPriceData = request.body.flightOfferPriceData;
+  const travelers = request.body.travelers;
+  const contacts = request.body.contacts;
 
   const payload = JSON.stringify({
     data: {
       type: "flight-order",
       flightOffers: [flightOfferPriceData],
-      travelers: [
-        {
-          id: "1",
-          dateOfBirth: "1982-01-16",
-          name: {
-            firstName: "JORGE",
-            lastName: "GONZALES",
-          },
-          gender: "MALE",
-          contact: {
-            emailAddress: "jorge.gonzales833@telefonica.es",
-            phones: [
-              {
-                deviceType: "MOBILE",
-                countryCallingCode: "34",
-                number: "480080076",
-              },
-            ],
-          },
-          documents: [
-            {
-              documentType: "PASSPORT",
-              birthPlace: "Madrid",
-              issuanceLocation: "Madrid",
-              issuanceDate: "2015-04-14",
-              number: "00000000",
-              expiryDate: "2025-04-14",
-              issuanceCountry: "ES",
-              validityCountry: "ES",
-              nationality: "ES",
-              holder: true,
-            },
-          ],
-        },
-        {
-          id: "2",
-          dateOfBirth: "2012-10-11",
-          gender: "FEMALE",
-          contact: {
-            emailAddress: "jorge.gonzales833@telefonica.es",
-            phones: [
-              {
-                deviceType: "MOBILE",
-                countryCallingCode: "34",
-                number: "480080076",
-              },
-            ],
-          },
-          name: {
-            firstName: "ADRIANA",
-            lastName: "GONZALES",
-          },
-        },
-        {
-          id: "3",
-          dateOfBirth: "2018-10-11",
-          gender: "FEMALE",
-          contact: {
-            emailAddress: "jorge.gonzales833@telefonica.es",
-            phones: [
-              {
-                deviceType: "MOBILE",
-                countryCallingCode: "34",
-                number: "480080076",
-              },
-            ],
-          },
-          name: {
-            firstName: "ADRIANA",
-            lastName: "GONZALES",
-          },
-        },
-        {
-          id: "4",
-          dateOfBirth: "2022-10-11",
-          gender: "FEMALE",
-          contact: {
-            emailAddress: "jorge.gonzales833@telefonica.es",
-            phones: [
-              {
-                deviceType: "MOBILE",
-                countryCallingCode: "34",
-                number: "480080076",
-              },
-            ],
-          },
-          name: {
-            firstName: "ADRIANA",
-            lastName: "GONZALES",
-          },
-        },
-      ],
+      travelers: travelers,
       remarks: {
         general: [
           {
@@ -339,35 +250,7 @@ app.post("/booking/flight-orders", async function (request, response) {
         option: "DELAY_TO_CANCEL",
         delay: "6D",
       },
-      contacts: [
-        {
-          addresseeName: {
-            firstName: "PABLO",
-            lastName: "RODRIGUEZ",
-          },
-          companyName: "INCREIBLE VIAJES",
-          purpose: "STANDARD",
-          phones: [
-            {
-              deviceType: "LANDLINE",
-              countryCallingCode: "34",
-              number: "480080071",
-            },
-            {
-              deviceType: "MOBILE",
-              countryCallingCode: "33",
-              number: "480080072",
-            },
-          ],
-          emailAddress: "support@increibleviajes.es",
-          address: {
-            lines: ["Calle Prado, 16"],
-            postalCode: "28014",
-            cityName: "Madrid",
-            countryCode: "ES",
-          },
-        },
-      ],
+      contacts: contacts,
     },
   });
 
